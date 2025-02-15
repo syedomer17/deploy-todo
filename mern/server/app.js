1854864469e5a2441187692a4ac03284de97d349
syedomer17 <syedomerali2006@gmail.com>
2025-02-15 18:32:04 +0530

    5010

--- mern/server/app.js
+++ mern/server/app.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = 5003;
+const PORT = 5010;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
